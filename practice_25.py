3883844f8b93475b3f5076f551fd68e24fb36977
radhikanandurkar <nandurkarradhika@gmail.com>
2026-03-02 22:58:24 +0530

    Create practice_25.py

---
+++ practice_25.py
@@ -0,0 +1,8 @@
+# Grade calculator (A/B/C).
+marks = int(input("enter your marks : "))
+if marks>=80:
+  print("grade is A")
+elif marks<80 and marks>=60:
+  print("grade is B ")
+else:
+  print("grade is C")
