0c9846da531d4aedd6aefaec545e72d525d44f3d
radhikanandurkar <nandurkarradhika@gmail.com>
2026-03-03 00:11:24 +0530

    Delete practice_25.py

--- practice_25.py
+++
@@ -1,8 +0,0 @@
-# Grade calculator (A/B/C).
-marks = int(input("enter your marks : "))
-if marks>=80:
-  print("grade is A")
-elif marks<80 and marks>=60:
-  print("grade is B ")
-else:
-  print("grade is C")
